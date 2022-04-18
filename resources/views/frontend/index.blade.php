e8f3a9dc1c1b2c0b855b4b9413ee9cb506c56207
Bipin Joshi <joshibipin2052@gmail.com>
2022-04-18 17:51:07 +0545

    api registration validation error message

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1354,13 +1354,16 @@ $time = date('Y-m-d H:i:a',$flash_deal->end_date);
         var data = @json($time);
         console.log(data);
         var countDownDate = new Date(data).getTime();
+        console.log('countDownDate'+countDownDate)
         // Update the count down every 1 second
         var x = setInterval(function() {
             // Get today's date and time
             var now = new Date().getTime();
+        console.log('now'+now)
             //   alert(countDownDate);
             // Find the distance between now and the count down date
             var distance = countDownDate - now;
+        console.log('distance'+distance)
             // Time calculations for days, hours, minutes and seconds
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
