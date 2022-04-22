4271d529f29121634b45c8dc6e76693aeb0c317d
Bipin Joshi <joshibipin2052@gmail.com>
2022-04-22 13:02:50 +0545

    changes map error

--- resources/views/frontend/payment/esewa.blade.php
+++ resources/views/frontend/payment/esewa.blade.php
@@ -18,11 +18,11 @@
     }
     
     function buildDataToSign($params) {
-            $signedFieldNames = explode(",",$params["signed_field_names"]);
-            foreach ($signedFieldNames as $field) {
-               $dataToSign[] = $field . "=" . $params[$field];
-            }
-            return commaSeparate($dataToSign);
+      $signedFieldNames = explode(",",$params["signed_field_names"]);
+      foreach ($signedFieldNames as $field) {
+          $dataToSign[] = $field . "=" . $params[$field];
+      }
+      return commaSeparate($dataToSign);
     }
     
     function commaSeparate ($dataToSign) {
@@ -57,50 +57,20 @@
       // 'amount' => $ordercode->grand_total,
       'amount' => '100',
       'currency' => 'NPR',
-      'submit' => ''
+      // 'submit' => ''
     ];
     // dd($params);
 
     ?>
     
     <form id="payment_confirmation" action="https://testsecureacceptance.cybersource.com/pay" method="post"><br/>
-      access_key : <input type="hidden1" name="access_key" value="cd7ac9c06b2b3bc8915cb8c08d2e2a93"><br/>
-      profile_id : <input type="hidden1" name="profile_id" value="AC9E8149-F889-4C78-893B-EAF207B3C7AC"><br/>
-      transaction_uuid : <input type="hidden1" name="transaction_uuid" value="<?php echo uniqid() ?>"><br/>
-      signed_field_names : <input type="hidden1" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency"><br/>
-      unsigned_field_names : <input type="hidden1" name="unsigned_field_names"><br/>
-      signed_date_time : <input type="hidden1" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>"><br/>
-      locale : <input type="hidden1" name="locale" value="en"><br/>
-  
-      auth_trans_ref_no : <input type="hidden1" name="auth_trans_ref_no"><br/>
-      bill_to_forename : <input type="hidden1" name="bill_to_forename" value="Rajim"><br/>
-      bill_to_surname : <input type="hidden1" name="bill_to_surname" value="Ali"><br/>
-      bill_to_email : <input type="hidden1" name="bill_to_email" value="ali.rajim12@gmail.com"><br/>
-      bill_to_phone : <input type="hidden1" name="bill_to_phone" value="9849428177"><br/>
-      bill_to_address_line1 : <input type="hidden1" name="bill_to_address_line1" value="Kathmandu"><br/>
-      bill_to_address_city : <input type="hidden1" name="bill_to_address_city" value="Kathmandu"><br/>
-      bill_to_address_state : <input type="hidden1" name="bill_to_address_state" value="Kathmandu"><br/>
-      bill_to_address_country : <input type="hidden1" name="bill_to_address_country" value="NP"><br/>
-      bill_to_address_postal_code : <input type="hidden1" name="bill_to_address_postal_code" value="Kathmandu"><br/>
-      bill_to_address_postal_code : <input type="hidden1" name="bill_to_address_postal_code" value="Kathmandu"><br/>
-
-      signature:<input type="hidden1" id="signature" name="signature" value="{{sign($params)}}"><br/>
-
-      bill_address1 :<input type="text" name="bill_address1" value="Nepal"> <br>
-      bill_city :<input type="text" name="bill_city" value="kathmandu"> <br>
-      bill_country <input type="text" name="bill_country" value="nepal"> <br>
-      customer_email <input type="text" name="customer_email" value="joshibipin2052@gmail.com"> <br>
-      customer_lastname :<input type="text" name="customer_lastname" value="joshi"> <br>
-
-
-      transaction_type : <input type="text" name="transaction_type" size="25" value="sale" ><br/>
-      reference_number : <input type="text" name="reference_number" size="25" value="<?php echo date('Y-m-dh:i');?>"><br/>
-      amount : <input type="text" name="amount" size="25" value="100"><br/>
-      {{-- {{$ordercode->grand_total}} --}}
-      currency : <input type="text" name="currency" size="25" value="NPR"><br/>
-      <input type="submit" id="submit" name="submit" value="Submit"/>
-      {{-- 9Jt/ShDl3KGGwnwvK7rw2FNmNaFde6fm5/MjZ7lFBuc= --}}
-      {{-- <button type="submit" class="submitnic"></button> --}}
+      <?php
+      foreach($params as $name => $value) {
+          echo $name." <input type=\"hidden1\" id=\"" . $name . "\" name=\"" . $name . "\" value=\"" . $value . "\"/>\n<br>";
+      }
+      echo "<input type=\"hidden1\" id=\"signature\" name=\"signature\" value=\"" . sign($params) . "\"/>\n";
+  ?>
+<input type="submit" id="submit" value="Confirm"/>
 
       
   </form>
@@ -151,26 +121,26 @@
           $("#"+payment_form).append(newbutton);
       }
 
-    function capitalize(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+      function capitalize(string) {
+          return string.charAt(0).toUpperCase() + string.slice(1);
+      }
 
-    function setDefaultsForPaymentDetailsSection() {
-        $("input[name='transaction_type']").val("authorization");
-        $("input[name='reference_number']").val(new Date().getTime());
-        $("input[name='amount']").val("100.00");
-        $("input[name='currency']").val("USD");
-    }
+      function setDefaultsForPaymentDetailsSection() {
+          $("input[name='transaction_type']").val("authorization");
+          $("input[name='reference_number']").val(new Date().getTime());
+          $("input[name='amount']").val("100.00");
+          $("input[name='currency']").val("USD");
+      }
 
-    function setDefaultsForUnsignedDetailsSection(){
-        $("input[name='card_type']").val("001");
-        $("input[name='card_number']").val("4242424242424242");
-        $("input[name='card_expiry_date']").val("11-2020");
-    }
+      function setDefaultsForUnsignedDetailsSection(){
+          $("input[name='card_type']").val("001");
+          $("input[name='card_number']").val("4242424242424242");
+          $("input[name='card_expiry_date']").val("11-2020");
+      }
 
 
 
-      // $('.submitnic').trigger('click');    
+      // $('#submit').trigger('click');    
     </script>
     
   </body>
