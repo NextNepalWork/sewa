109cc181c51f77acc0a5e5950f3c90604a2f5829
sujan-sht <sujanstha016@gmail.com>
2022-05-05 18:17:10 +0545

    faq in api

--- app/Http/Controllers/Api/PolicyController.php
+++ app/Http/Controllers/Api/PolicyController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Faq;
 use App\Http\Resources\PolicyCollection;
+use App\Http\Resources\FaqCollection;
 use App\Models\Policy;
 use Illuminate\Http\Request;
 
@@ -30,5 +32,10 @@ class PolicyController extends Controller
     public function privacy()
     {
         return new PolicyCollection(Policy::where('name', 'privacy_policy')->get());
+    }
+
+    public function faq()
+    {
+        return new FaqCollection(Faq::all());
     }
 }
